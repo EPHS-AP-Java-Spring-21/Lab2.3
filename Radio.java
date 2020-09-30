bcedd3d21e9263a137272a0943d9ff67363be9f6
MrMayRRHS <32204762+MrMayRRHS@users.noreply.github.com>
2020-09-29 19:13:52 -0500

    Add files via upload

---
+++ Radio.java
@@ -0,0 +1,10 @@
+/* Write the class Radio
+ * 
+ * Instance variable for frequency (double freq)
+ * 
+ * void setStation(double station)
+ * double getStation()
+ * void changeStation(double amnt)
+ * void nextStation() +0.2
+ * void prevStation() -0.2
+ */
---
+++ RadioTester.java
@@ -0,0 +1,26 @@
+public class RadioTester
+{
+  public static void main(String[] args)
+  {
+    //Radio r = new Radio();
+    //System.out.println(r.getStation()); // should be 0.0
+    
+    //r.setStation(95.5);
+    //System.out.println(r.getStation()); // should be 95.5
+    
+    //r.changeStation(1.4);
+    //System.out.println(r.getStation()); // should be 95.7
+    
+    //r.nextStation();
+    //System.out.println(r.getStation()); // should be 95.9
+    
+    //r.nextStation();
+    //System.out.println(r.getStation()); // should be 96.1
+    
+    //r.prevStation();
+    //r.prevStation();
+    //r.prevStation();
+    //System.out.println(r.getStation()); // should be 95.5
+  }
+}
+  
