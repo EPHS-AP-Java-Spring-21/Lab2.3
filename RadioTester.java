9026592a491988a30e20473f1fa84d5dc1e2d33c
MrMayRRHS <32204762+MrMayRRHS@users.noreply.github.com>
2020-10-06 09:56:49 -0500

    Update RadioTester.java

--- RadioTester.java
+++ RadioTester.java
@@ -9,18 +9,18 @@ public class RadioTester
     //System.out.println(r.getStation()); // should be 95.5
     
     //r.changeStation(1.4);
-    //System.out.println(r.getStation()); // should be 95.7
+    //System.out.println(r.getStation()); // should be 96.9
     
     //r.nextStation();
-    //System.out.println(r.getStation()); // should be 95.9
+    //System.out.println(r.getStation()); // should be 97.1
     
     //r.nextStation();
-    //System.out.println(r.getStation()); // should be 96.1
+    //System.out.println(r.getStation()); // should be 97.3
     
     //r.prevStation();
     //r.prevStation();
     //r.prevStation();
-    //System.out.println(r.getStation()); // should be 95.5
+    //System.out.println(r.getStation()); // should be 96.7
   }
 }
   
